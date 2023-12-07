e83610b5675c54c4d75a0727381b5258fd37c9c1
eslam_alhendawy <eslam_alhendawy@outlook.com>
2023-12-07 07:43:40 +0200

    Fixed Base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Weather-App/"
+  base: "/Weather-App2.0/"
 })
